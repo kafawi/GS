0cb40ee742b71719b82a2f4deeba79057b26f811
kafawi <derkalle@web.de>
2015-10-26 14:17:00 +0400

    implement search-methodes

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ struct Person {
 };
 
 int main (void) {
+	int index;
 	float jammer;
 	struct Person p[5]={
 		{"Hugo",   1500.12, 4711},
@@ -27,7 +28,15 @@ int main (void) {
 		
 	// Implementierung der binären Suche
 
-	
+	index = lin_search(p,5,4713);
+	jammer = p[index].gehalt;
+	//print jammer;
+
+	jammer=0.1;
+
+	index = bin_search(p,5,4713);
+	jammer = p[index].gehalt;
+	// print "bin" jammer;
 
 
 	// 	Hier kommt der KOT
@@ -40,7 +49,32 @@ int main (void) {
 /**********************************+** EOF ************************************/
 
 /**
- * Lineare Suche auf einem Array von Personen. 
+ * Lineare Suche auf einem Array von Personen.
+ * 
+ * @ param a Array
+ * @ param size Laenge des Arrays
+ * @ param key Sucheschluessel
+ *
+ * @ retrun >= 0: gefunden
+ *          <  0: nicht gefunden
+ */
+
+int lin_search(const Personen a[], const int size, const int key ){
+	
+	int i;
+	
+	for (i=0; i < size ; i++){
+		if(a[i].pnr == key){
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+
+/**
+ * Binaere Suche auf einem Array von Personen. 
  *
  * @ param a Personarray
  * @ param size Arraylaenge
@@ -51,10 +85,22 @@ int main (void) {
  */
 
 int bin_search( const Person a[], const int size, const int key ){
+	int links = 0;
+	int rechts = size - 1;
+	int mitte = 0
+	int ergebnis = -1;
 
-x=(l+r)/2
-
-
-
+	while (ergebnis - 1 || links > rechts ){
+		mitte = ( links + rechts ) / 2 ;
+		if ( a[ mitte ].pnr ){
+			ergebnis = mitte;
+			return ergebnis;
+		}else if ( key < a[ mitte ].pnr  ){
+			rechts = mitte - 1;
+		}else {
+			links = mitte + 1;
+		}
+	}
+	return ergebnis;
 }
 
